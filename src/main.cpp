e564f795b8ec108c843bd56f86de82268e3f96d3
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-02-24 19:07:18 +0000

    Refactor Player class

--- src/main.cpp
+++ src/main.cpp
@@ -100,6 +100,13 @@ class ScoreDisplay {
 
 class Player {
     public:
+        Player(int starting_x, int starting_y, bn::fixed player_speed, bn::size player_size) :
+            sprite(bn::sprite_items::dot.create_sprite(starting_x, starting_y)),
+            speed(player_speed),
+            size(player_size),
+            bounding_box(create_bounding_box(sprite, size))
+        {}
+
         /**
          * Update the position and bounding box of the player based on d-pad movement.
          */
@@ -116,7 +123,7 @@ class Player {
         }
 
         // Create the sprite. This will be moved to a constructor
-        bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
+        bn::sprite_ptr sprite; // Sprite pointer
         bn::fixed speed; // The speed of the player
         bn::size size; // The width and height of the sprite
         bn::rect bounding_box; // The rectangle around the sprite for checking collision
@@ -130,12 +137,7 @@ int main() {
 
     // Create a player and initialize it
     // TODO: we will move the initialization logic to a constructor.
-    Player player = Player();
-    player.sprite.set_x(44);
-    player.sprite.set_y(22);
-    player.speed = 1.5;
-    player.size = PLAYER_SIZE;
-    player.bounding_box = create_bounding_box(player.sprite, player.size);
+    Player player = Player(-19, 22, 2.0, PLAYER_SIZE);
 
     bn::sprite_ptr enemy_sprite = bn::sprite_items::square.create_sprite(-30, 22);
     bn::rect enemy_bounding_box = create_bounding_box(enemy_sprite, ENEMY_SIZE);
@@ -153,7 +155,6 @@ int main() {
         // Update the scores and disaply them
         scoreDisplay.update();
         
-
         bn::core::update();
     }
 }
