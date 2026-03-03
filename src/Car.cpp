3b5eeec8a86ef80b5e36ba4592d3fc32c03136ea
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-03-03 00:34:27 +0000

    Car changes orientation depending on movement

--- src/Car.cpp
+++ src/Car.cpp
@@ -29,16 +29,20 @@ void Car::update()
     if (sprite.y() <= MIN_Y)
     {
         sprite.set_y(MIN_Y);
+        sprite.set_vertical_flip(true); // changing car orientation to face proper direction
         if (speed < 0)
         {
             speed = -speed;
+
         }
     }
     else if (sprite.y() >= MAX_Y)
     {
         sprite.set_y(MAX_Y);
+        sprite.set_vertical_flip(false); // changing car orientation to face proper direction
         if (speed > 0)
         {
+           
             speed = -speed;
         }
     }
